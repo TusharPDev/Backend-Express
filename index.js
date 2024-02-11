a31f985469d89c3e099970abcd5e15728e8e8ef4
TusharPDev <97661665+TusharPDev@users.noreply.github.com>
2024-02-11 18:18:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hey, hello this server listens to tushar!");
+});
+
+app.listen(3000, () => {
+  console.log("Express server initialized");
+});
