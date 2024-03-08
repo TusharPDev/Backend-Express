3b91c831fdb4d36d2c0e6951237b228f70844db0
Tushar shamkant patil <97661665+TusharPDev@users.noreply.github.com>
2024-03-08 17:25:29 +0000

    middleware/modules

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+
+//This is a middle ware function
+const myLoggerMiddleware = function (req, res, next) {
+  console.log("LOGGED");
+  next();
+};
+
+app.use(myLoggerMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Hey, hello this server listens to tushar!");
 });
@@ -9,6 +18,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Express server initialized");
 });
-
-
-
